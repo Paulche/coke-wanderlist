81a968ec5d61a2d848d0ffdb0c0ba2840b0a55c9
Paul Chechetin <paulche@yandex.ru>
2014-04-11 13:17:13 +0400

    Initial Commit

---
+++ Journey/CokeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CokeAppDelegate.h
+//  Journey
+//
+//  Created by Pavel Chechetin on 4/11/14.
+//  Copyright (c) 2014 Pavel Chechetin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CokeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Journey/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Journey
+//
+//  Created by Pavel Chechetin on 4/11/14.
+//  Copyright (c) 2014 Pavel Chechetin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CokeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CokeAppDelegate class]));
+    }
+}
